f065bf89994d4014302c3720f8de88ae1c7c4af9
Wondbob <68884704+Wondbob@users.noreply.github.com>
2024-01-20 18:19:55 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,111 @@
+// document.querySelectorAll('.image-container').forEach(image =>{
+// 	image.onclick = () =>{
+// 		document.querySelector('.popup-image').style.display = 'block';
+// 		document.querySelector('.popup-image img').src = image.getAttribute('src');
+// 	}
+// })
+
+// document.querySelector('.popup-image, span').onclick = () =>{
+// 	document.querySelector('.popup-image').style.display = 'none';
+// }
+
+
+
+
+
+
+// Popup 1
+function img1() {
+	document.getElementById('pop1').style.display = 'block';
+}
+function cancel1() {
+	document.getElementById('pop1').style.display = "none";
+}
+
+// Popup 2
+function img2() {
+	document.getElementById('pop2').style.display = 'block';
+}
+function cancel2() {
+	document.getElementById('pop2').style.display = "none";
+}
+
+// Popup 3
+function img3() {
+	document.getElementById('pop3').style.display = 'block';
+}
+function cancel3() {
+	document.getElementById('pop3').style.display = "none";
+}
+
+// Popup 4
+function img4() {
+	document.getElementById('pop4').style.display = 'block';
+}
+function cancel4() {
+	document.getElementById('pop4').style.display = "none";
+}
+
+// Popup 5
+function img5() {
+	document.getElementById('pop5').style.display = 'block';
+}
+function cancel5() {
+	document.getElementById('pop5').style.display = "none";
+}
+
+// Popup 6
+function img6() {
+	document.getElementById('pop6').style.display = 'block';
+}
+function cancel6() {
+	document.getElementById('pop6').style.display = "none";
+}
+
+// Popup 7
+function img7() {
+	document.getElementById('pop7').style.display = 'block';
+}
+function cancel7() {
+	document.getElementById('pop7').style.display = "none";
+}
+
+// Popup 8
+function img8() {
+	document.getElementById('pop8').style.display = 'block';
+}
+function cancel8() {
+	document.getElementById('pop8').style.display = "none";
+}
+
+// Popup 9
+function img9() {
+	document.getElementById('pop9').style.display = 'block';
+}
+function cancel9() {
+	document.getElementById('pop9').style.display = "none";
+}
+
+// Popup 10
+function img10() {
+	document.getElementById('pop10').style.display = 'block';
+}
+function cancel10() {
+	document.getElementById('pop10').style.display = "none";
+}
+
+// Popup 11
+function img11() {
+	document.getElementById('pop11').style.display = 'block';
+}
+function cancel11() {
+	document.getElementById('pop11').style.display = "none";
+}
+
+// Popup 12
+function img12() {
+	document.getElementById('pop12').style.display = 'block';
+}
+function cancel12() {
+	document.getElementById('pop12').style.display = "none";
+}
